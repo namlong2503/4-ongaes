38f0f5c9d775e8fc3878864e747b806be97c829a
namlong2503 <114210195+namlong2503@users.noreply.github.com>
2022-10-06 10:49:03 +0700

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: '4Oranges',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
